eb532b5a58c9f50810efec83afa7f35088a8fde6
UlysseGebe <Uly35534e31>
2019-04-19 15:41:42 +0200

    fullscreen

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const $carousels = document.querySelectorAll('.js-carousel')
 
-for(const $carousel of $carousels) {
+for (const $carousel of $carousels) {
     const carousel = new HeticCarousel($carousel)
 }
 
@@ -8,3 +8,29 @@ const $image = document.querySelectorAll('.img')
 for (let i = 0; i < $image.length; i++) {
     $image[i].style.backgroundImage = `url("image/image_0${i+1}.jpg")`
 }
+
+const key = () => {
+    let screen = document.querySelector('.js-carousel')
+    let keyResult = event.charCode
+    if (keyResult == 102) {
+        if (document.fullscreenElement == null) {
+            screen.requestFullscreen()
+        } else {
+            document.exitFullscreen()
+        }
+    }
+}
+
+const check = () => {
+    let sibling = document.querySelectorAll('.sibling')
+    for (let i = 0; i < sibling.length; i++) {
+        if (document.fullscreenElement == null) {
+            sibling[i].style.display = 'none'
+        } else {
+            sibling[i].style.display = 'flex'
+        }
+    }
+}
+
+window.addEventListener('keypress', key)
+window.addEventListener('keypress', check)
